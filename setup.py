20f3391635a13980e17ca68dbb455734a2726ca1
xyz <desotanalomolatachi@gmail.com>
2024-03-22 11:43:55 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
-
+requirements = []
 setuptools.setup(
     name='discord_check',
     version='0.0.1',
-    install_requires=open('requirements.txt').read().splitlines(),
+    install_requires=requirements,
     packages=setuptools.find_packages()
 )
