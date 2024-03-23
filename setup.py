b5a78bf3ebcd14ac4a1ccb13e5ae0864b60b231c
xyz <desotanalomolatachi@gmail.com>
2024-03-23 21:25:38 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setuptools.setup(
     name='discord_check',
     version='0.0.1',
     install_requires=requirements,
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
