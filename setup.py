d5ce139205b00aa3a3955c7e4b074961ba68f2e4
xyz <desotanalomolatachi@gmail.com>
2024-04-08 14:50:48 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ import setuptools
 requirements = []
 setuptools.setup(
     name='discord_check',
-    version='1.0.2',
+    version='1.0.3',
     install_requires=requirements,
+    description = 'A module check token discord active',
     packages=setuptools.find_packages(),
     long_description=open('README').read(),
     long_description_content_type='text/markdown',
